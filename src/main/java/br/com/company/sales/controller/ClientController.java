713e3b881b0905fcd549b39a03cf0b2956a34e0a
Tiago Pereira <tiagopereiracx@gmail.com>
2022-01-23 21:24:29 -0300

    refatorando controller

--- src/main/java/br/com/company/sales/controller/ClientController.java
+++ src/main/java/br/com/company/sales/controller/ClientController.java
@@ -5,9 +5,10 @@ import br.com.company.sales.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
-import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,60 +22,45 @@ public class ClientController {
         this.clientRepository = clientRepository;
     }
 
-//    @GetMapping
-//    public ResponseEntity<List<Client>> getAll(){
-//        List<Client> clients = clientRepository.findAll();
-//        if(clients.isEmpty()){
-//            return ResponseEntity.notFound().build();
-//        }
-//        return ResponseEntity.ok(clients);
-//
-//    }
-
     @GetMapping("/{id}")
-    public ResponseEntity<Client> getById(@PathVariable Integer id){
-        Optional<Client> client = clientRepository.findById(id);
-        if(client.isPresent()){
-            return ResponseEntity.ok(client.get());
-        }
-        return ResponseEntity.notFound().build();
+    public Client getById(@PathVariable Integer id){
+        return clientRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente inexistente!"));
     }
 
     @GetMapping
-    public ResponseEntity find(Client clientFilters){
+    public List<Client> find(Client clientFilters){
         ExampleMatcher exampleMatcher = ExampleMatcher.matching()
                                         .withIgnoreCase()
                                         .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
-        Example example = Example.of(clientFilters, exampleMatcher);
-        List<Client> clients = clientRepository.findAll(example);
-        return ResponseEntity.ok(clients);
+        Example<Client> example = Example.of(clientFilters, exampleMatcher);
+        return clientRepository.findAll(example);
     }
 
     @PostMapping
-    public ResponseEntity save(@RequestBody Client client){
-        Client clientSaved = clientRepository.save(client);
-        return ResponseEntity.ok(clientSaved);
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client save(@RequestBody Client client){
+        return clientRepository.save(client);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity update(@PathVariable Integer id, @RequestBody Client client){
-        Optional<Client> clientUpdate = clientRepository.findById(id);
-        if(clientUpdate.isPresent()){
-            client.setId(id);
-            clientRepository.save(client);
-            return ResponseEntity.ok(client);
-        }
-
-        return ResponseEntity.badRequest().body(null);
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable Integer id, @RequestBody Client client){
+        clientRepository.findById(id).map(
+            clientUpdated -> {
+                client.setId(id);
+                clientRepository.save(client);
+                return client;
+            }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente inexistente!"));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Client> delete(@PathVariable Integer id){
-        Optional<Client> client = clientRepository.findById(id);
-        if(client.isPresent()){
-            clientRepository.delete(client.get());
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.notFound().build();
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id){
+        clientRepository.findById(id).map(
+                client -> {
+                    clientRepository.delete(client);
+                    return client;
+                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente inexistente!"));
     }
 }
