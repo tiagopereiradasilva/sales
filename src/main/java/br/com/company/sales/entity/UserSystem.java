542293e3794039f768f19bd4abce53d5e3372d13
Tiago Pereira <tiagopereiracx@gmail.com>
2022-02-11 23:36:22 -0300

    Adição da entidade UserSystem

---
+++ src/main/java/br/com/company/sales/entity/UserSystem.java
@@ -0,0 +1,20 @@
+package br.com.company.sales.entity;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class UserSystem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String username;
+    private String password;
+    private boolean admin;
+}
