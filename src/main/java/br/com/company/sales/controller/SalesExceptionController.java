df34b2fc5ae04f9c7857cd3b32a491e2f5426305
Tiago Pereira <tiagopereiracx@gmail.com>
2022-03-02 11:30:26 -0300

    Alteração para modelo de resposta do ResponseTemplate

--- src/main/java/br/com/company/sales/controller/SalesExceptionController.java
+++ src/main/java/br/com/company/sales/controller/SalesExceptionController.java
@@ -1,10 +1,9 @@
 package br.com.company.sales.controller;
 
 import br.com.company.sales.exception.SalesException;
-import br.com.company.sales.exception.StandardErrorResponse;
+import br.com.company.sales.rest.dto.ResponseTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,58 +11,49 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.Instant;
-import java.util.List;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class SalesExceptionController {
 
+    private ResponseTemplate response;
+    private String messageResponse = "Ops! Algo deu errado";
     @ExceptionHandler(SalesException.class)
-    public ResponseEntity<StandardErrorResponse> salesException(SalesException exception, HttpServletRequest request){
-        StandardErrorResponse standardErrorResponse = new StandardErrorResponse(exception.getMessage());
-        standardErrorResponse.setTimestamp(Instant.now());
-        standardErrorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-        //TODO pensar melhor nesse título.
-        //standardErrorResponse.setError("Title error");
-        //standardErrorResponse.setMessage(new ApiErrors());
-        standardErrorResponse.setPath(request.getRequestURI());
-
-        return ResponseEntity.status(standardErrorResponse.getStatus()).body(standardErrorResponse);
+    public ResponseEntity<ResponseTemplate> salesException(SalesException exception, HttpServletRequest request){
+        var errors = exception.getMessage();
+        response = new ResponseTemplate(errors, messageResponse);
+        response.setError(true);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(response);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<StandardErrorResponse> handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception){
-        List<String> erros = exception
+    public ResponseEntity<ResponseTemplate> handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        var errors = exception
                                 .getBindingResult()
                                 .getAllErrors()
                                 .stream()
                                 .map(
                                         error -> error.getDefaultMessage()
                                 ).collect(Collectors.toList());
-
-        StandardErrorResponse standardErrorResponse = new StandardErrorResponse(erros);
-        standardErrorResponse.setTimestamp(Instant.now());
-        standardErrorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-        return ResponseEntity.status(standardErrorResponse.getStatus()).body(standardErrorResponse);
+        response = new ResponseTemplate(errors, messageResponse);
+        response.setError(true);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(response);
     }
 
     @ExceptionHandler(UsernameNotFoundException.class)
-    public ResponseEntity<StandardErrorResponse> usernameNotFoundException(UsernameNotFoundException exception){
-        String erros = exception.getMessage();
-        StandardErrorResponse standardErrorResponse = new StandardErrorResponse(erros);
-        standardErrorResponse.setTimestamp(Instant.now());
-        standardErrorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-        return ResponseEntity.status(standardErrorResponse.getStatus()).body(standardErrorResponse);
+    public ResponseEntity<ResponseTemplate> usernameNotFoundException(UsernameNotFoundException exception){
+        var errors = exception.getMessage();
+        response = new ResponseTemplate(errors, messageResponse);
+        response.setError(true);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(response);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<StandardErrorResponse> httpMessageNotReadableException(HttpMessageNotReadableException exception){
-        String erros = exception.getLocalizedMessage();
-        StandardErrorResponse standardErrorResponse = new StandardErrorResponse(erros);
-        standardErrorResponse.setTimestamp(Instant.now());
-        standardErrorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-        return ResponseEntity.status(standardErrorResponse.getStatus()).body(standardErrorResponse);
+    public ResponseEntity<ResponseTemplate> httpMessageNotReadableException(HttpMessageNotReadableException exception){
+        var errors = exception.getLocalizedMessage();
+        response = new ResponseTemplate(errors, messageResponse);
+        response.setError(true);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(response);
     }
 
 
