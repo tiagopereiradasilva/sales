3efc38a0148ffb9fda373570bb4b16fdc6d8f54b
Tiago Pereira <tiagopereiracx@gmail.com>
2022-03-02 11:29:44 -0300

    Adição de classe para executar rotinas comuns a todo o sistema

---
+++ src/main/java/br/com/company/sales/util/Helper.java
@@ -0,0 +1,11 @@
+package br.com.company.sales.util;
+
+public class Helper {
+    public static String toLowerCase(String value){
+        return value.toLowerCase();
+    }
+
+    public static String toUpperCase(String value){
+        return value.toUpperCase();
+    }
+}
